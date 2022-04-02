9c561116d862708ec4d265e4c965d78d618e6ac6
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-02 12:32:37 +0530

    [Mahesh]First_Commit

---
+++ WelcomeEmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeEmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
