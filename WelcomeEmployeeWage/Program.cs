fa6995a5dc9d87d769659fe09526985a8688bf74
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-02 13:58:03 +0530

    [Mahesh]EmployeeWage_Using_Switch

--- WelcomeEmployeeWage/Program.cs
+++ WelcomeEmployeeWage/Program.cs
@@ -6,29 +6,30 @@ using System.Threading.Tasks;
 
 namespace WelcomeEmployeeWage
 {
-    internal class Add_Part_time_Employee
+    class UC4_Employee_Wage_Using_Switch
     {
+        public const int isFullTime = 1;     // Constants
+        public const int isPartTime = 2;     // Constants
+        public const int empRatePerHour = 20;    // Constants
+    
+    
         static void Main(string[] args)
         {
-            Console.WriteLine("UC3_Add_Part_Time_Employee\n");
-            int isFullTime = 1;     // Constants
-            int isPartTime = 2;     // Constants
-            int empRatePerHour = 20;    // Constants
             int empHrs = 0;     // varibles
             int empWage = 0;    // varibles
             Random random = new Random();   // Instantiate random number generator using supplied value as seed.
             int empCheck = random.Next(0,3);    // Generate 3 random integer values(0,1,2)
-            if (empCheck == isFullTime)     // using if Statement
+             switch (empCheck)      // using Switch Statement
             {
-                empHrs = 8;
-            }
-            else if (empCheck == isPartTime) 
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case isFullTime:
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * empRatePerHour;
             Console.WriteLine("Emp Wage : " + empWage);
