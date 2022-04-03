2c77a895a81999aa183f8b3ff6866065e377012d
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-03 14:35:17 +0530

    [Mahesh]Calculating_Wage_for_a_Month

--- WelcomeEmployeeWage/Program.cs
+++ WelcomeEmployeeWage/Program.cs
@@ -6,33 +6,41 @@ using System.Threading.Tasks;
 
 namespace WelcomeEmployeeWage
 {
-    class UC4_Employee_Wage_Using_Switch
+    class UC5_Calculating_Wages_for_a_Month
     {
         public const int isFullTime = 1;     // Constants
         public const int isPartTime = 2;     // Constants
         public const int empRatePerHour = 20;    // Constants
-    
-    
+        public const int numOfWorkingDays = 20;     //Constants
+
+
         static void Main(string[] args)
         {
             int empHrs = 0;     // varibles
             int empWage = 0;    // varibles
-            Random random = new Random();   // Instantiate random number generator using supplied value as seed.
-            int empCheck = random.Next(0,3);    // Generate 3 random integer values(0,1,2)
-             switch (empCheck)      // using Switch Statement
+            int totalEmpWage = 0;   // Varibles
+            for (int day = 0; day < numOfWorkingDays; day++) // Using for loop 
             {
-                case isFullTime:
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();   // Instantiate random number generator using supplied value as seed.
+                int empCheck = random.Next(0, 3);    // Generate 3 random integer values(0,1,2)
+                switch (empCheck)      // using Switch Statement
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * empRatePerHour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * empRatePerHour;
-            Console.WriteLine("Emp Wage : " + empWage);
-        }
+            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
+        } 
     }
+
 }
